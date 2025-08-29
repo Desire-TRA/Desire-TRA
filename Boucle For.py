f81c2afd429c0f5408625517d267dee5d1b651bb
Désiré TRA <desiretra88@gmail.com>
2025-08-29 17:01:57 +0000

    nombres = input("Veuillez saisir une liste de nombres séparés par des virgules : ")
    nombres_liste = nombres.split(',')
    
    nombres_liste = [int(nombre) for nombre in nombres_liste]
    
    for nombre in nombres_liste:
        print(nombre)
    
    moyenne = sum(nombres_liste) / len(nombres_liste)
    print("La moyenne des nombres est :", moyenne)
    
    count_sup_moyenne = sum(1 for nombre in nombres_liste if nombre > moyenne)
    print("Le nombre de nombres supérieurs à la moyenne est :", count_sup_moyenne)

---
+++ Boucle For.py
@@ -0,0 +1,16 @@
+#races_de_chien = ["golden retriever", "chihuahua", "terrier", "carlin"]
+#for cocorico in races_de_chien:
+#    print(cocorico)
+
+#Boucle For
+#for x in range(5):
+ #   print(x)
+
+#for x in range(4, 10):
+#    print(f"{x} bouteilles de bières au mur !")
+
+#Boucle While
+capacité_maximale = 10
+capacité_actuelle = 3
+while capacité_actuelle < capacité_maximale:
+    capacité_actuelle += 2
