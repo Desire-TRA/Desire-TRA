34656f39111f8aa7f62688095ef7940784056a0e
Désiré TRA <desiretra88@gmail.com>
2025-09-02 22:23:52 +0000

    https://github.com/OpenClassrooms-Student-Center/7168871-apprenez-les-bases-du-langage-python.git

--- Boucle For.py
+++ Boucle For.py
@@ -10,7 +10,27 @@
 #    print(f"{x} bouteilles de bières au mur !")
 
 #Boucle While
-capacité_maximale = 10
-capacité_actuelle = 3
-while capacité_actuelle < capacité_maximale:
-    capacité_actuelle += 2
+#capacité_maximale = 10
+#capacité_actuelle = 3
+#while capacité_actuelle < capacité_maximale:
+#    capacité_actuelle += 2
+
+#x = 0
+#while x != 5:
+#    x += 2
+
+#Instructions break et continue
+#for i in range(10):
+#    if i == 5:
+#        break
+#    print(i)
+
+#"liste = [1, 2, 3, 4, 5]
+# Boucle for sur la liste
+#for element in liste:
+#    if element == 3:
+#        # Si l'élément vaut 3, on passe à l'itération suivante sans exécuter le reste du code
+#        continue
+    # Dans tous les autres cas, on exécute le reste du code
+ #   print(element)
+
---
+++ LES FONCTIONS.py
@@ -0,0 +1,33 @@
+#def message():
+ #   print("Bonjour tout le monde !")
+
+
+#message()
+
+
+#def afficher_nom_prenom(nom, prenom):
+ #   print("Nom :", nom)
+  #  print("Prénom :", prenom)
+
+
+#afficher_nom_prenom("Doe", "John")
+
+
+#def addition(a, b):
+ #   resultat = a + b
+  #  print("Le résultat de l'addition est :", resultat)
+
+
+#param1 = 20
+#param2 = 50
+
+#addition(param1, param2)
+
+#def multiplication(x, y):
+  #  resultat = x * y
+ #   print(resultat)
+
+
+#multiplication(5, 10)
+
+
